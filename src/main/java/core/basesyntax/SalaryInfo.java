e69da82e14ed6f47c3950be56f39fd15e273e7c0
Denys <werdumbox@gmail.com>
2026-06-30 09:46:07 +0200

    fixed magic numbers and .lineSeparator added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int RATE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder salaryInfo = new StringBuilder();
@@ -16,23 +20,23 @@ public class SalaryInfo {
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo)
-                .append("\n");
+                .append(System.lineSeparator());
 
         for (String name : names) {
             int salary = 0;
             for (String datum : data) {
                 String[] parts = datum.split("\\s+");
-                LocalDate currentDate = LocalDate.parse(parts[0], DATE_TIME_FORMATTER);
-                if (name.equals(parts[1])) {
+                LocalDate currentDate = LocalDate.parse(parts[DATE_INDEX], DATE_TIME_FORMATTER);
+                if (name.equals(parts[NAME_INDEX])) {
                     if (!currentDate.isBefore(from) && !currentDate.isAfter(to)) {
-                        salary += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                        salary += Integer.parseInt(parts[HOURS_INDEX]) * Integer.parseInt(parts[RATE_INDEX]);
                     }
                 }
             }
             salaryInfo.append(name)
                     .append(" - ")
                     .append(salary)
-                    .append("\n");
+                    .append(System.lineSeparator());
         }
         return salaryInfo.toString();
     }
